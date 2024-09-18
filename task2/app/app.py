a3e4f9bb87bfbb9bf8a14d3c8965a470160029f0
liminfinity <108136720+liminfinity@users.noreply.github.com>
2024-09-19 00:12:10 +0700

    feat(task2): add colorama

--- task2/app/app.py
+++ task2/app/app.py
@@ -2,6 +2,8 @@ from app.lib.file import File
 from app.lib.args import args
 from app.lib.figure_action import FigureAction
 from app.lib.filter import filter_figures
+from colorama import Fore, Style
+
 
 class App:
     def __init__(self):
@@ -19,10 +21,11 @@ class App:
 
     def __print_figures(self):
         figures = filter_figures(self.file.read())
-        print('Фигуры:')
+        print(Fore.LIGHTGREEN_EX + 'Фигуры:' + Style.RESET_ALL)
         for idx, figure in enumerate(figures):
             print(f'{idx + 1}. {figure}')
 
     def __count_figures(self):
         figures = filter_figures(self.file.read())
-        print(len(figures))
+        figuresCnt = str(len(figures))
+        print(f'Количество фигур: {Fore.LIGHTGREEN_EX + figuresCnt + Style.RESET_ALL}')
