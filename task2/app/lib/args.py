92227ff04cb727f61f30df39041d3be1ab4c5784
liminfinity <108136720+liminfinity@users.noreply.github.com>
2024-09-23 13:09:20 +0700

    feat(task2): add version

--- task2/app/lib/args.py
+++ task2/app/lib/args.py
@@ -3,8 +3,10 @@ from app.lib.figure_action import FigureAction
 
 parser = ArgumentParser(description='CLI для управления фигурами')
 
-parser.add_argument('-f', '--file', metavar='filename', type=str, default='input.txt', help='Имя файла с фигурами')
+parser.add_argument('-f', '--file', metavar='filename', type=str, required=True, help='Имя файла с фигурами')
 
 parser.add_argument('-o', '--oper', metavar='command', type=FigureAction, default=FigureAction.PRINT, help='Операция над фигурами')
+
+parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
 
 args = parser.parse_args()
